d4c1b338c9951fd7e5b6797898590d97ac6ed6dd
keiske0408 <kieske0408@gmail.com>
2025-08-29 11:18:09 +0800

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_28_163037) do
+  create_table "tests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
